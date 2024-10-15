a0afd4cbefc8e6f9b57bbe5ea6c71ef1cbd1904f
YosikiNosiki <yosefyakubov770@gmail.com>
2024-10-15 09:11:49 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+function showMessage() {
+    alert("Спасибо за ваше сообщение!");
+}
+
+function showMessage() {
+    alert("Спасибо за ваше сообщение!");
+}
+function showMessage() {
+    alert("Спасибо за ваше сообщение!"); 
+}
+
